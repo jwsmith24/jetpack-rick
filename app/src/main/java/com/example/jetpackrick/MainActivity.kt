4e41af8b2a973ce718a01d6a7e192c7ac10610d1
Jacob Smith <jacob.w.smith97.mil@army.mil>
2026-01-29 15:37:56 -0600

    add hilt dependency, use view model to display characters
    - #9

--- app/src/main/java/com/example/jetpackrick/MainActivity.kt
+++ app/src/main/java/com/example/jetpackrick/MainActivity.kt
@@ -6,18 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.jetpackrick.data.network.JetpackRickApi
 import com.example.jetpackrick.data.repository.CharacterRepository
+import com.example.jetpackrick.ui.character.CharacterViewModel
 import com.example.jetpackrick.ui.theme.JetpackRickTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import androidx.hilt.navigation.compose.hiltViewModel
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -28,24 +35,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launch {
 
-            try {
-                characterRepository.printCharacters()
-            } catch(exception: Exception) {
-                exception.printStackTrace()
-            }
-        }
 
 
         enableEdgeToEdge()
         setContent {
+
             JetpackRickTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val viewModel: CharacterViewModel = hiltViewModel()
+                    val characters by viewModel.characters.collectAsState(initial = emptyList())
+
+                    LazyColumn (modifier = Modifier.padding(innerPadding)) {
+                        items(characters) {
+                            Text(it.name)
+                        }
+                    }
                 }
             }
         }
