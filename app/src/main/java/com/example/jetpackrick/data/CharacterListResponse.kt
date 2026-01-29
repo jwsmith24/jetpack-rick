36160e6d8598f3d3c6dd1fe33fbbf1a2bdcc0ed1
Jacob Smith <jacob.w.smith97.mil@army.mil>
2026-01-29 10:55:46 -0600

    add models for character list response and character
    - #2

---
+++ app/src/main/java/com/example/jetpackrick/data/CharacterListResponse.kt
@@ -0,0 +1,15 @@
+package com.example.jetpackrick.data
+
+data class CharacterListResponse(
+    val info: Info,
+    val results: List<Character>
+)
+
+data class Info(
+    val count: Int,
+    val pages: Int,
+    val next: String,
+    val prev: String
+)
+
+
