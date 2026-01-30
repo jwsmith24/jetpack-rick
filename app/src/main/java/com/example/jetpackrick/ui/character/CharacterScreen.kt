73eb4f8cc0c1d7a1f387a305c9b7c800a633d365
Jacob Smith <jacob.w.smith97.mil@army.mil>
2026-01-30 13:54:07 -0600

    pull character screen into own module

--- app/src/main/java/com/example/jetpackrick/ui/character/CharacterScreen.kt
+++ app/src/main/java/com/example/jetpackrick/ui/character/CharacterScreen.kt
@@ -8,10 +8,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
-import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemKey
 import com.example.jetpackrick.data.CharacterResponse
