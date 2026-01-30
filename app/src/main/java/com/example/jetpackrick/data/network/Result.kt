56dcfc2d4c6250f94426b146f4134f6f1c37da12
Jake <155990103+jwsmith24@users.noreply.github.com>
2026-01-30 10:22:35 -0600

    adjust character layout (#14)
    * add models for character list response and character
    
    - #2
    
    * add json adapters to dto data classes
    
    - #2
    
    * create moshi instance, okhttp instance with interceptor, and retrofit instance
    
    - create interface with annotations to guide retrofit implementation
    - #3
    
    * add api interface, add get all and get by id methods
    
    - #4
    
    * add repo methods to fetch characters and to print response
    
    - #5
    
    * add repo class and fetch characters in main on startup
    
    - add nullability to info data class
    - change client from class to singleton object
    - #5
    
    * refactor for di with hilt
    
    - #6
    
    * add hilt dependency, use view model to display characters
    
    - #9
    
    * implement sealed classes, update view model to use them
    
    - add loading indicator
    
    - #10
    
    * add box layout to entire character screen
    
    - add header
    - pull out constants to viewmodel companion object
    - #11
    
    ---------
    
    Co-authored-by: Jacob Smith <jacob.w.smith97.mil@army.mil>

---
+++ app/src/main/java/com/example/jetpackrick/data/network/Result.kt
@@ -0,0 +1,9 @@
+package com.example.jetpackrick.data.network
+
+import com.example.jetpackrick.data.CharacterResponse
+
+sealed class Result<out T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+}
